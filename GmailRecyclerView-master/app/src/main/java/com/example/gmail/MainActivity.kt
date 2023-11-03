892bf1747629f7a68ca4a1296dfd20643fba151c
Nguyen-Duc-Tuan <83194648+Nguyen-Duc-Tuan@users.noreply.github.com>
2023-11-03 20:35:37 +0700

    Add files via upload

---
+++ GmailRecyclerView-master/app/src/main/java/com/example/gmail/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.gmail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Lấy dữ liệu từ FakeDataSource.kt
+        val mailList = FakeDataSource().getListMail()
+
+        // Tạo Adapter cung cấp dữ liệu cho listView
+        val recyclerViewAdapter = MailAdapter(mailList)
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
+        val linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = recyclerViewAdapter
+    }
+}
